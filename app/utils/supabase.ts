47f153da99e163d03df468c13067940ad19aaa5e
Vedant Goyal <vg5952@gmail.com>
2025-04-27 16:38:58 +0530

    Connected my code with supabase

---
+++ app/utils/supabase.ts
@@ -0,0 +1,15 @@
+import Constants from 'expo-constants'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl    = Constants.expoConfig?.extra?.supabaseUrl
+const supabaseAnonKey= Constants.expoConfig?.extra?.supabaseAnonKey
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false
+  }
+})
